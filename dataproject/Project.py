51aeadc6d0affd4aff9ec1e6adada2190f0119c8
Alexander Laustsen <hcv700@alumni.ku.dk>
2019-04-01 14:31:55 +0200

    Ny

--- dataproject/Project.py
+++ dataproject/Project.py
@@ -6,8 +6,7 @@ import matplotlib.pyplot as plt
 Dst = pydst.Dst(lang='en')
 Dst.get_data(table_id='INDKP101')
 Vars = Dst.get_variables(table_id='INDKP101')
-
-Everything = Dst.get_data(table_id = 'INDKP101', variables={'OMRÅDE':['*'], 'KOEN':['M','K'], 'TID':['*'], 'INDKOMSTTYPE':['100']}).rename(columns={'OMRÅDE':'Municipality'})
+Everything = Dst.get_data(table_id = 'INDKP101', variables={'OMRÅDE':['000','11'], 'KOEN':['M','K'], 'TID':['*'], 'ENHED':['116'], 'INDKOMSTTYPE':['100']}).rename(columns={'OMRÅDE':'Municipality'})
 #Changing the index to Municipality:
 New_index = Everything.set_index('Municipality')
 #Picking out the neccesary variables:
@@ -21,14 +20,16 @@ Women_without_year = Women[['Gender', 'Disposal income women']]
 Concatenated_table = pd.concat([Men, Women_without_year], axis=1)
 #Removing the gender nicer look:
 Final_table = Concatenated_table[['Year','Disposal income men','Disposal income women']]
+Final_table
 #Creates a function with provides the difference between the genders in %:
 def f(x):
     """Gives the procentual difference between the genders"""
-    return (x['Disposal income women']/x['Disposal income men']-1)*100
+    return (x['Disposal income men']/x['Disposal income women']-1)*100
 Final_table['Difference in %']=Final_table.apply(f, axis=1)
 Final_table.loc['All Denmark'].head()
 #Example on finding the numbers for All Denmark:
 Whole_country = Final_table.loc['All Denmark']
+Final_table['Province']
 
 #Nice graf
 plt.plot(Whole_country['Year'],Whole_country['Difference in %'])
@@ -39,6 +40,7 @@ plt.grid(True)
 plt.show()
 
 
+Copenhagen = Final_table.loc['Copenhagen']
 def standard_for_men(Municipality):
     """Creates a standard normal distribution for men's disposal income in the chosen municipality"""
     s = np.random.normal(Municipality['Disposal income men'].mean(), Municipality['Disposal income men'].std(), 10000)
@@ -52,3 +54,5 @@ def standard_for_women(Municipality):
     count, bins, ignored = plt.hist(s, 30, density=True)
     plt.plot(bins, 1/(Municipality['Disposal income women'].std() *np.sqrt(2*np.pi)) * np.exp(-(bins-Municipality['Disposal income women'].mean())**2 / (2 * Municipality['Disposal income women'].std()**2)), linewidth = 2)
     return plt.show
+
+
