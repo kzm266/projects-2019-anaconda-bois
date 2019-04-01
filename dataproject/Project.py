de1c4605079a4eb327476784f4ff1699bb3b3a41
Alexander Laustsen <hcv700@alumni.ku.dk>
2019-04-01 10:40:26 +0200

    Ny

--- dataproject/Project.py
+++ dataproject/Project.py
@@ -3,39 +3,38 @@ import pandas as pd
 import pydst
 import matplotlib.pyplot as plt
 
-Dst = pydst.Dst(lang='da')
+Dst = pydst.Dst(lang='en')
 Dst.get_data(table_id='INDKP101')
 Vars = Dst.get_variables(table_id='INDKP101')
 
-Alt = Dst.get_data(table_id = 'INDKP101', variables={'OMRÅDE':['*'], 'KOEN':['M','K'], 'TID':['*'], 'INDKOMSTTYPE':['100']})
-#Skifter index til OMRÅDE i stedet for:
-Alti = Alt.set_index('OMRÅDE')
-#Udvælger ting så de andre ting ikke kommer med:
-Sorteret = Alti[['TID','KOEN','INDHOLD']].rename(columns={'KOEN':'Køn', 'TID':'År', 'INDHOLD':'Disp. indkomst'})
-#Laver en tabel for hvert køn seperat:
-Mænd = Sorteret[Sorteret['Køn']=='Mænd'].sort_values(['OMRÅDE','År']).rename(columns={'Disp. indkomst':'Disp. indkomst mænd'})
-Kvinder = Sorteret[Sorteret['Køn']=='Kvinder'].sort_values(['OMRÅDE', 'År']).rename(columns={'Disp. indkomst':'Disp. indkomst kvinder'})
-#Skal ikke have år med 2 gange når vi sammensætter:
-SortKvinder = Kvinder[['Køn', 'Disp. indkomst kvinder']]
-#Sammensæt de to tabeller:
-Pænt = pd.concat([Mænd, SortKvinder], axis=1)
-#Fjern køn #NoGenderAssuming
-Endnu_pænere = Pænt[['År','Disp. indkomst mænd','Disp. indkomst kvinder']]
-#Funktion der giver forskellen i pct.
+Everything = Dst.get_data(table_id = 'INDKP101', variables={'OMRÅDE':['*'], 'KOEN':['M','K'], 'TID':['*'], 'INDKOMSTTYPE':['100']})
+#Changing the index to AREA:
+New_index = Everything.set_index('OMRÅDE')
+#Picking out the neccesary variables:
+Sortet = New_index[['TID','KOEN','INDHOLD']].rename(columns={'KOEN':'Gender', 'TID':'Year', 'INDHOLD':'Disposal income'})
+#Making a table for each gender:
+Men = Sortet[Sortet['Gender']=='Men'].sort_values(['OMRÅDE','Year']).rename(columns={'Disposal income':'Disposal income men'})
+Women = Sortet[Sortet['Gender']=='Women'].sort_values(['OMRÅDE', 'Year']).rename(columns={'Disposal income':'Disposal income women'})
+#We don't want year to appear twice when we concat:
+Women_without_year = Women[['Gender', 'Disposal income women']]
+#Concatenate the two tables:
+Concatenated_table = pd.concat([Men, Women_without_year], axis=1)
+#Removing the gender nicer look:
+Final_table = Concatenated_table[['Year','Disposal income men','Disposal income women']]
+#Creates a function with provides the difference between the genders in %:
 def f(x):
-    return (x['Disp. indkomst kvinder']/x['Disp. indkomst mænd']-1)*100
-Endnu_pænere['Forskel i %']=Endnu_pænere.apply(f, axis=1)
-Endnu_pænere.loc['Hele landet'].head()
-#Eksempel på at finde tal fra København
-København = Endnu_pænere.loc['København']
-#For hele landet:
-Hele_Landet = Endnu_pænere.loc['Hele landet']
+    """Gives the procentual difference between the genders"""
+    return (x['Disposal income women']/x['Disposal income men']-1)*100
+Final_table['Difference in %']=Final_table.apply(f, axis=1)
+Final_table.loc['All Denmark'].head()
+#Example on finding the numbers for All Denmark:
+Whole_country = Final_table.loc['All Denmark']
 
-#Fed graf
-plt.plot(København['År'],København['Forskel i %'])
-plt.xlabel('År')
-plt.ylabel('Procentvis forskel')
-plt.title('Forskel i disponibel indkomst for København')
+#Nice graf
+plt.plot(Whole_country['Year'],Whole_country['Difference in %'])
+plt.xlabel('Year')
+plt.ylabel('Difference in %')
+plt.title('Difference in disposal income for all Denmark')
 plt.grid(True)
 plt.show()
 
@@ -47,9 +46,9 @@ def standard(mu, sigma):
     plt.plot(bins, 1/(sigma *np.sqrt(2*np.pi)) * np.exp(-(bins-mu)**2 / (2 * sigma**2)), linewidth = 2)
     return plt.show
 
-#Finder normalfordelingen for København og hele landet:
-København.mean()
-København.std()
-Hele_Landet.mean()
-Hele_Landet.std()
+#Finding the normal distribution for Copenhagen and all Denmark:
+Copenhagen.mean()
+Copenhagen.std()
+Whole_country.mean()
+Whole_country.std()
 plt.show(standard(205228.58, 16782.75), standard(214377.2, 82925.37))
