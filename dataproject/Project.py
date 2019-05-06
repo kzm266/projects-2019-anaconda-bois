c659a5a112870c7d8b1738eb782701bc10e53b6e
tnv875 <tnv875@ku.alumni.dk>
2019-05-06 14:13:57 +0200

    Import Ipwidgets

--- dataproject/Project.py
+++ dataproject/Project.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import pydst
 import matplotlib.pyplot as plt
+from ipywidgets import interact, interactive, fixed, interact_manual
 import ipywidgets as widgets
 
 #DATACLEANING#
