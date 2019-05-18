b7dc76e82d08a5f65854f969c71575b3c5fdfc50
tnv875 <tnv875@ku.alumni.dk>
2019-05-19 00:09:05 +0200

    Viderebyggelse på edu

--- dataproject/uddannelse.py
+++ dataproject/uddannelse.py
@@ -32,20 +32,51 @@ Men = Sortet_mun[Sortet_mun['Gender']=='Men'].sort_values(['Municipality','Year'
 Women = Sortet_mun[Sortet_mun['Gender']=='Women'].sort_values(['Municipality', 'Year','Edulvl']).rename(columns={'disposable income':'disposable_income_women'})
 
 #Merge tables
-merged_table = pd.merge(Men, Women, on=['Year','Edulvl', 'Municipality'], how='outer')
+#merged_table = pd.merge(Men, Women, on=['Year','Edulvl', 'Municipality'], how='outer')
 
 #Final
 #Final_table = 
 
-
-
 #We don't want year to appear twice when we concat:
-#Women_without_year = Women[['Gender', 'disposable_income_women']]
+Women_without_year = Women[['Gender', 'disposable_income_women']]
 
 #Concatenate the two tables:
-#Concatenated_table = pd.concat([Men, Women_without_year], axis=1)
+Concatenated_table = pd.concat([Men, Women_without_year], axis=1)
 
 #Remove gender
-#Final_table = Concatenated_table[['disposable_income_men','disposable_income_women']]
+Final_table = Concatenated_table[['Edulvl','disposable_income_men','disposable_income_women']]
 
 #TEST
+
+#Creates a function with provides the difference between the genders in %:
+def f(x):
+    """Gives the procentual difference between the genders"""
+    return round((x['disposable_income_men']/x['disposable_income_women']-1)*100, 2)
+
+#Applying the function to the end of the table:
+Final_table['Difference in %']=Final_table.apply(f, axis=1)
+
+#We now wish to create a individual table for each province, and do it like this:
+#We start by finding the unique values in the table AKA all the provinces
+unik_mun = Final_table.index.unique()
+
+#DET ENESTE DER SKAL OPDATERES ER EN DIC DER PASSER MED BÅDE MUN OG EDU
+
+#Making an empty dictionary which will contain our unique values with their seperate table later
+d = {}
+
+#Filling the empty dictionary
+for i in unik:
+    d.update( {i : Final_table.loc[i]})
+
+
+#We can now plot the difference between men and women in a graph like this:
+def Difference(Region, Edulvl):
+    #Simply plotting the difference against years to see the evolution
+    plt.plot(d[Region, Edulvl]['Year'],d[Region, Edulvl]['Difference in %'])
+    plt.xlabel('Year')
+    plt.ylabel('Difference in %')
+    plt.title(f'Difference in disposable_income for {str(Region, Edulvl)}')
+    plt.axis([2004,2018,9,32.5])
+    plt.grid(True)
+    return plt.show()
